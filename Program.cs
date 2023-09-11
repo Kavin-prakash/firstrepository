98a6b864192f99d8b04ce2db2168a84194c655b6
Kavin Prakash Senthil Prabhu <kavin.senthil@itp.objectfrontier.com>
2023-09-11 11:43:43 +0530

    array

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
+using System;
 Console.WriteLine("hello world");
+
+class Prgram
+{
+   public static void Main(string[] args)
+   {
+    int [] a= {1,2,3,4,5,};
+    foreach (int i in a)
+    {
+        console.WriteLine(i);
+    }
+   }
+}
